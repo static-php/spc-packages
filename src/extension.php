4cba28559dfba2fa276955b0aa3d1b3aa0b05ea9
DubbleClick <m@pyc.ac>
2025-06-02 18:12:34 +0700

    move extensions to ini/extension dir

--- src/extension.php
+++ src/extension.php
@@ -2,26 +2,37 @@
 
 namespace staticphp;
 
+use SPC\store\Config;
+
 class extension implements package
 {
     public function __construct(
-        private string $name
+        private readonly string $name
     ) {
 
     }
 
     public function getFpmConfig(): array
     {
+        $config = Config::getExt($this->name);
+        if (!$config) {
+            throw new \Exception("Extension configuration for '{$this->name}' not found.");
+        }
+        $depends = ['static-php'];
+        foreach ($config['ext-depends'] ?? [] as $dep) {
+            $depends[] = 'php-' . $dep;
+        }
         return [
             'config-files' => [
                 '/etc/static-php/php.d/'. $this->name . '.ini',
             ],
-            'depends' => [
-                'static-php'
+            'provides' => [
+                'php-' . $this->name,
             ],
+            'depends' => $depends,
             'files' => [
                 BUILD_MODULES_PATH . '/' . $this->name . '.so' => '/usr/lib/static-php/modules/' . $this->name . '.so',
-                INI_PATH . '/' . $this->name . '.ini' => '/etc/static-php/php.d/' . $this->name . '.ini',
+                INI_PATH . '/extension/' . $this->name . '.ini' => '/etc/static-php/php.d/' . $this->name . '.ini',
             ]
         ];
     }
